86b2446b7fc89e423de1d1226526ba7ce53896c4
Arun <arunvrvce@gmail.com>
2019-10-06 23:03:47 +0530

    inital commit

---
+++ cmake-build-debug/CMakeFiles/NinjaDB.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/NinjaDB.dir/main.c.o"
+  "NinjaDB"
+  "NinjaDB.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/NinjaDB.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
